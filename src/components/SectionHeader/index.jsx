716cf798d6bc2a7b1f3bd4d481d568029e168701
Adam Jałocha <jalochadev@gmail.com>
2022-07-23 08:59:43 +0200

    Removed unnecessary refs in SectionHeader

--- src/components/SectionHeader/index.jsx
+++ src/components/SectionHeader/index.jsx
@@ -1,38 +1,35 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Typewriter from 'typewriter-effect';
 import { Header } from './index.style';
 
-const SectionHeader = React.forwardRef(({ inView, children }, ref) => {
-  const cursor = useRef(null);
-  const container = useRef(null);
-
-  return (
-    <Header ref={ref}>
-      {inView && (
+const SectionHeader = React.forwardRef(({ inView, children }, ref) => (
+  <Header ref={ref}>
+    {inView && (
       <Typewriter
         onInit={(typewriter) => {
           typewriter
-            .callFunction(({ elements }) => {
-              container.current = elements.container;
-              cursor.current = elements.cursor;
-            })
             .changeDelay(50)
             .typeString(children)
             .callFunction(
-              () => cursor.current.classList.add('hide-cursor'),
+              ({ elements }) => {
+                const { cursor } = elements;
+                cursor.classList.add('hide-cursor');
+              },
             )
             .pauseFor(700)
             .callFunction(
-              () => container.current.classList.add('show-background'),
+              ({ elements }) => {
+                const { container } = elements;
+                container.classList.add('show-background');
+              },
             )
             .start();
         }}
       />
-      )}
-    </Header>
-  );
-});
+    )}
+  </Header>
+));
 
 SectionHeader.propTypes = {
   inView: PropTypes.bool.isRequired,
