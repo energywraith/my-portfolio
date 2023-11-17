83347477ca0ce1e410daeb08afb1de5e6edc87d7
energywraith <energywraith@gmail.com>
2023-11-17 20:46:58 +0100

    Moved Drugie Zycie flyer to be first

--- src/components/templates/Designs/useDesigns.js
+++ src/components/templates/Designs/useDesigns.js
@@ -4,6 +4,12 @@ import HarmoniaJPG from 'assets/harmonia-design.jpg';
 
 const useDesigns = () => [
   {
+    intlName: 'designs.drugieZycie',
+    image: DrugieZycieJPG,
+    figmaLink: 'https://www.figma.com/file/1i6E1yI0MNBLCEiCwh9cOq/Ulotki?type=design&node-id=22-401&mode=design&t=ClikpUqt4AiIVjwk-4',
+    demoLink: DrugieZycieJPG,
+  },
+  {
     intlName: 'designs.harmonia',
     image: HarmoniaJPG,
     figmaLink: 'https://www.figma.com/file/RmhNGOgpAsbVAoiO3RUJGN/Harmonia?type=design&node-id=0%3A1&mode=design&t=Eo1tDYfaovf62FVe-1',
@@ -14,12 +20,6 @@ const useDesigns = () => [
     image: ShelterJPG,
     figmaLink: 'https://www.figma.com/file/1i6E1yI0MNBLCEiCwh9cOq/Ulotki?type=design&node-id=22-8&mode=design&t=gxd2B765NlfmHGk6-4',
     demoLink: ShelterJPG,
-  },
-  {
-    intlName: 'designs.drugieZycie',
-    image: DrugieZycieJPG,
-    figmaLink: 'https://www.figma.com/file/1i6E1yI0MNBLCEiCwh9cOq/Ulotki?type=design&node-id=22-401&mode=design&t=ClikpUqt4AiIVjwk-4',
-    demoLink: DrugieZycieJPG,
   },
 ];
 
